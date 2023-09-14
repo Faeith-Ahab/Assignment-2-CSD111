d2c10b31a2c9bbbbdfb453db5d45a733e51f0aa2
Faeith-Ahab <143060249+Faeith-Ahab@users.noreply.github.com>
2023-09-14 20:21:44 +0800

    lists

---
+++ .py
@@ -0,0 +1,33 @@
+# List 60, 80, 90, 98
+
+# 1.  Change 80 to 89 in the list.
+
+student_marks = [ 60, 80, 90, 98 ] 
+student_marks [1] = 89
+print(student_marks)
+
+
+# 2.  Add a new item in the list, 55.
+student_marks = [ 60, 80, 90, 98 ]
+student_marks.append(55)
+print(student_marks)
+
+
+# 3.  Write a python program to sum all the items in the list.
+
+total = sum(student_marks)
+print(total)
+
+
+# 4.  Find the size of the list having appended item 55.
+print(len(student_marks))
+
+
+# 5.  Write a python function that takes two lists and returns true if they have atleast one common member.
+A = input("Enter your items:")
+B = input("Enter your item:")
+print("True, there is common members in A and B")
+
+
+
+
